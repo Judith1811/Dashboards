062795e51a54494b988a33213ef4893e9fb6baaf
Judith1811 <judith.paassen@study.hs-duesseldorf.de>
2022-09-01 10:01:50 +0200

    hochladen

---
+++ Henkel/1_📈_Status Quo Persil.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import re
+import numpy as np
+import plotly.graph_objs as go
+
+st.set_page_config(page_title='Amazon Suchvolumen Persil')
+st.title('Amazon Suchvolumen Persil')
+st.write('Für folgende Auswertung den aktuellen Report aus Vendor Central (https://vendorcentral.amazon.de/analytics/dashboard/searchTerms) herunterladen. '
+         'Auswertungsbereich frei wählbar und Abteilung "Amazon.de" oder "Drugstore" möglich.')
+
+uploaded_file = st.file_uploader('Report aus Vendor Central hier hochladen:', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = new_header
+    df.rename(columns=df.iloc[0])
+    df.drop(df.index[1])
+
+    st.write('Auswertung der Suchbegriffe erfolgt auf der Ebene', df.iloc[0]['Abteilung'],'.')
+
+    #Hilfestellungen###################################################################################
+    dsi = df.Suchbegriff.str.contains('persil |persil$', flags = re.IGNORECASE, regex = True, na = False)
+    toppersil = df[dsi]
+    toppersil = toppersil.dropna()
+
+    dsi = df.Suchbegriff.str.contains('ariel |ariel$', flags = re.IGNORECASE, regex = True, na = False)
+    topariel = df[dsi]
+    topariel = topariel.dropna()
+    ###################################################################################################
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        "####  Ranking Suchbegriffe rund um Persil:"
+        dropdown_persil = toppersil['Suchbegriff'].values.tolist()
+        optionpersil = st.selectbox('Wähle einen Suchbegriff',dropdown_persil)
+        persil = df.loc[(df['Suchbegriff'] == optionpersil)]
+        persil.reset_index(drop=True,inplace=True)
+        st.write('Der Suchbegriff',optionpersil, 'belegt im Ranking Platz', persil.loc[0]['Suchfrequenz-Rang '],'von',df['Suchfrequenz-Rang '].iat[-1])
+
+    with col2:
+        "####  Ranking Suchbegriffe rund um Ariel:"
+        dropdown_ariel = topariel['Suchbegriff'].values.tolist()
+        optionariel = st.selectbox('Wähle einen Suchbegriff',dropdown_ariel)
+        ariel = df.loc[(df['Suchbegriff'] == optionariel)]
+        ariel.reset_index(drop=True,inplace=True)
+        st.write('Der Suchbegriff',optionariel, 'belegt im Ranking Platz', ariel.loc[0]['Suchfrequenz-Rang '],'von',df['Suchfrequenz-Rang '].iat[-1])
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric(label=optionpersil, value=persil.loc[0]['Suchfrequenz-Rang '], delta=ariel.loc[0]['Suchfrequenz-Rang ']-persil.loc[0]['Suchfrequenz-Rang '])
+
+    with col2:
+        st.metric(label=optionariel, value=ariel.loc[0]['Suchfrequenz-Rang '], delta=persil.loc[0]['Suchfrequenz-Rang ']-ariel.loc[0]['Suchfrequenz-Rang '])
+
+    '#### TOP Suchbegriffe rund um Persil:'
+    st.dataframe(toppersil)
+
+    '#### TOP Suchbegriffe rund um Ariel:'
+    st.dataframe(topariel)
+
+    '#### TOP Suchbegriffe rund um Persil und Ariel:'
+    dsi = df.Suchbegriff.str.contains('ariel |persil |persil$|ariel$', flags = re.IGNORECASE, regex = True, na = False)
+    toppersilariel = df[dsi]
+    toppersilariel = toppersilariel.dropna()
+    st.dataframe(toppersilariel)
+
+    '#### Persil Produkte unter den TOP 3 der angeklickten ASINs:'
+    df.rename(columns={"Produkttitel #1": "col1","Produkttitel #2": "col2","Produkttitel #3": "col3"},inplace=True)
+    ids_persil = df.col1.str.contains('persil |persil$', flags = re.IGNORECASE, regex = True, na = False)
+    product1_persil = df[ids_persil]
+    ids_persil = df.col2.str.contains('persil |persil$', flags = re.IGNORECASE, regex = True, na = False)
+    product2_persil = df[ids_persil]
+    ids_persil = df.col3.str.contains('persil |persil$', flags = re.IGNORECASE, regex = True, na = False)
+    product3_persil = df[ids_persil]
+    products_persil = pd.concat([product1_persil,product2_persil,product3_persil]).drop_duplicates().reset_index(drop=True)
+    products_persil = products_persil.dropna()
+    st.dataframe(products_persil)
+
+    '#### Ariel Produkte unter den TOP 3 der angeklickten ASINs:'
+    ids_ariel = df.col1.str.contains('ariel |ariel$', flags = re.IGNORECASE, regex = True, na = False)
+    product1_ariel = df[ids_ariel]
+    ids_ariel = df.col2.str.contains('ariel ariel$', flags = re.IGNORECASE, regex = True, na = False)
+    product2_ariel = df[ids_ariel]
+    ids_ariel = df.col3.str.contains('ariel ariel$', flags = re.IGNORECASE, regex = True, na = False)
+    product3_ariel = df[ids_ariel]
+    products_ariel = pd.concat([product1_ariel,product2_ariel,product3_ariel]).drop_duplicates().reset_index(drop=True)
+    products_ariel = products_ariel.dropna()
+    st.dataframe(products_ariel)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.caption('Generische Suchbegriffe, bei denen unter den TOP 3 geklickten Produkten nur Persil auftaucht und kein Ariel')
+        vergleich_persil = products_persil.iloc[: , [1]]
+        vergleich_persil['Marke'] = 'Persil'
+        vergleich_persil = vergleich_persil[~vergleich_persil.Suchbegriff.str.contains("persil ")]
+        vergleich_persil = vergleich_persil[~vergleich_persil.Suchbegriff.str.contains(" persil")]
+        vergleich_persil = vergleich_persil[vergleich_persil.Suchbegriff != 'persil']
+        vergleich_persil.reset_index(inplace=True, drop=True)
+        vergleich_persil
+
+    with col2:
+        st.caption('Generische Suchbegriffe, bei denen unter den TOP 3 geklickten Produkten nur Ariel auftaucht und kein Persil')
+        vergleich_ariel = products_ariel.iloc[: , [1]]
+        vergleich_ariel['Marke'] = 'Ariel'
+        vergleich_ariel = vergleich_ariel[~vergleich_ariel.Suchbegriff.str.contains("ariel ")]
+        vergleich_ariel = vergleich_ariel[~vergleich_ariel.Suchbegriff.str.contains(" ariel")]
+        vergleich_ariel = vergleich_ariel[vergleich_ariel.Suchbegriff != 'ariel']
+        vergleich_ariel.reset_index(inplace=True, drop=True)
+        vergleich_ariel
+
+    title = st.text_input('Suchbegriff', 'Trage hier den gewünschten Suchbegriff ein')
+    '#### TOP 3 Klickrate des Suchbegriffs'
+    waschmittelpods = df.loc[(df['Suchbegriff'] == title)]
+    waschmittelpods1 = waschmittelpods[['col1','Klickrate #1']]
+    waschmittelpods1.rename(columns={'col1':'Produktname','Klickrate #1':'Klickrate'},inplace=True)
+    waschmittelpods2 = waschmittelpods[['col2','Klickrate #2']]
+    waschmittelpods2.rename(columns={'col2':'Produktname','Klickrate #2':'Klickrate'},inplace=True)
+    waschmittelpods3 = waschmittelpods[['col3','Klickrate #3']]
+    waschmittelpods3.rename(columns={'col3':'Produktname','Klickrate #3':'Klickrate'},inplace=True)
+    waschmittelpodssum = pd.concat([waschmittelpods1, waschmittelpods2, waschmittelpods3]).drop_duplicates().reset_index(drop=True)
+    waschmittelpodssum['Produkt'] = waschmittelpodssum['Produktname'].str.split().str[:4].str.join(sep=" ")
+    waschmittelpodssum = waschmittelpodssum.drop('Produktname',axis=1)
+    waschmittelpodssum['Klickrate']=pd.to_numeric(waschmittelpodssum['Klickrate'])
+    waschmittelpodssum.loc['sum']=1-waschmittelpodssum.sum(numeric_only=True, axis=0)
+    waschmittelpodssum.replace(np.nan,'Others',inplace=True)
+    labels = waschmittelpodssum['Produkt'].value_counts().index
+    values = waschmittelpodssum['Klickrate'].values
+    fig = go.Figure(
+        go.Pie(
+            labels = labels,
+            values = values,
+            hoverinfo = "label+percent",
+            textinfo = "percent"
+        ))
+    st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+
+#%%
+
+#%%
---
+++ Henkel/pages/2_📈_Entwicklung.py
@@ -0,0 +1,126 @@
+import os
+import pandas as pd
+import streamlit as st
+import numpy as np
+import plotly.express as px
+cwd = os.path.abspath('')
+import re
+import altair as alt
+#%%
+st.set_page_config(page_title='Entwicklung von Suchbegriffen im Vergleich')
+st.title('Entwicklung von Suchbegriffen im Vergleich')
+st.write('Für folgende Auswertung die Mastertabelle (...) um die ersten 50.000 Zeilen der aktuellen KW ergänzen, und mit dem ensprechenden Zeistempel '
+         'ergänzen. Aktuelle Reporte stammen aus Vendor Central: https://vendorcentral.amazon.de/analytics/dashboard/searchTerms Auswertungsbereich: '
+         'wöchentlich, Abteilung: Amazon.de')
+#%%
+uploaded_file = st.file_uploader('Report aus Vendor Central hier hochladen:', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = new_header
+    df.rename(columns=df.iloc[0])
+    df.drop(df.index[1])
+
+    dsi = df.Suchbegriff.str.contains('persil |persil$', flags = re.IGNORECASE, regex = True, na = False)
+    df1 = df[dsi]
+
+    dropdown_persil = df1['Suchbegriff'].values.tolist()
+    temp_list = []
+    for i in dropdown_persil:
+        if i not in temp_list:
+            temp_list.append(i)
+    dropdown_persil = temp_list
+
+    '#### Wähle einen Suchbegriff 1'
+    optionpersil = st.selectbox('Suchbegriff 1:',dropdown_persil)
+    persilpowerbar= df1.loc[(df1['Suchbegriff'] == optionpersil)]
+    #persilpowerbar = persilpowerbar.iloc[: , :-1]
+    #%%
+    ranking1 = persilpowerbar.iloc[:, [2,15]]
+    #ranking1['Woche'] = ranking1.iloc[:,1].copy()
+    #ranking1['Woche'] = ranking1['Woche'].map(lambda x: str(x)[:-5])
+    #ranking1 = ranking1.iloc[:,[0,2]]
+    ranking1.rename(columns={"Suchfrequenz-Rang ": "Suchbegriff 1"},inplace=True)
+    ranking1['Suchbegriff 1']*= -1
+
+    '#### Ranking des Suchbegriffs 1'
+    st.line_chart(data=ranking1, x='Woche', y='Suchbegriff 1')
+
+######################
+    entwicklungppb1 = persilpowerbar.iloc[:, [4,5,15]]
+    entwicklungppb1.rename(columns={"Produkttitel #1": "Produkttitel","Klickrate #1": "Klickrate"},inplace=True)
+
+    entwicklungppb2 = persilpowerbar.iloc[:, [8,9,15]]
+    entwicklungppb2.rename(columns={"Produkttitel #2": "Produkttitel","Klickrate #2": "Klickrate"},inplace=True)
+
+    entwicklungppb3 = persilpowerbar.iloc[:, [12,13,15]]
+    entwicklungppb3.rename(columns={"Produkttitel #3": "Produkttitel","Klickrate #3": "Klickrate"},inplace=True)
+
+    entwicklungppb = pd.concat([entwicklungppb1, entwicklungppb2, entwicklungppb3]).drop_duplicates().reset_index(drop=True)
+    entwicklungppb = entwicklungppb.dropna()
+    entwicklungppb['Produkttitel'] = entwicklungppb['Produkttitel'].str.split(',').str[0]
+
+    entwicklungppb=entwicklungppb.pivot(index='Woche',columns='Produkttitel',values='Klickrate')
+
+    entwicklungppb = entwicklungppb.fillna(0)
+
+    entwicklungppb['Rest']=1-entwicklungppb.sum(numeric_only=True, axis=1)
+
+    '#### Verteilung der TOP 3 geklickten Produkte von Suchbegriff 1'
+    st.bar_chart(data=entwicklungppb)
+
+######################
+
+    dropdown_persil2 = dropdown_persil
+
+    '#### Wähle einen Suchbegriff 2'
+    optionpersil2 = st.selectbox('Suchbegriff 2:',dropdown_persil2)
+    persilpowerbar2= df1.loc[(df1['Suchbegriff'] == optionpersil2)]
+    #persilpowerbar2 = persilpowerbar2.iloc[: , :-1]
+
+    ranking2 = persilpowerbar2.iloc[:, [2,15]]
+    #ranking2['Woche'] = ranking2.iloc[:,1].copy()
+    #ranking2['Woche'] = ranking2['Woche'].map(lambda x: str(x)[:-5])
+    #ranking2 = ranking2.iloc[:,[0,2]]
+    ranking2.rename(columns={"Suchfrequenz-Rang ": "Suchbegriff 2"},inplace=True)
+    ranking2['Suchbegriff 2']*= -1
+
+    ranking = pd.merge(ranking1, ranking2, on=['Woche'])
+    ranking = ranking[['Woche','Suchbegriff 1','Suchbegriff 2']]
+    ranking.set_index('Woche')
+
+    '#### Ranking des Suchbegriffs 2'
+    st.line_chart(data=ranking2, x='Woche', y='Suchbegriff 2')
+
+######################
+
+    entwicklungppb1b = persilpowerbar2.iloc[:, [4,5,15]]
+    entwicklungppb1b.rename(columns={"Produkttitel #1": "Produkttitel","Klickrate #1": "Klickrate"},inplace=True)
+
+    entwicklungppb2b = persilpowerbar2.iloc[:, [8,9,15]]
+    entwicklungppb2b.rename(columns={"Produkttitel #2": "Produkttitel","Klickrate #2": "Klickrate"},inplace=True)
+
+    entwicklungppb3b = persilpowerbar2.iloc[:, [12,13,15]]
+    entwicklungppb3b.rename(columns={"Produkttitel #3": "Produkttitel","Klickrate #3": "Klickrate"},inplace=True)
+
+    entwicklungppb_b = pd.concat([entwicklungppb1b, entwicklungppb2b, entwicklungppb3b]).drop_duplicates().reset_index(drop=True)
+    entwicklungppb_b = entwicklungppb_b.dropna()
+    entwicklungppb_b['Produkttitel'] = entwicklungppb_b['Produkttitel'].str.split(',').str[0]
+
+    entwicklungppb_b=entwicklungppb_b.pivot(index='Woche',columns='Produkttitel',values='Klickrate')
+
+    entwicklungppb_b = entwicklungppb_b.fillna(0)
+
+    entwicklungppb_b['Rest']=1-entwicklungppb_b.sum(numeric_only=True, axis=1)
+
+    '#### Verteilung der TOP 3 geklickten Produkte von Suchbegriff 2'
+    st.bar_chart(data=entwicklungppb_b)
+
+######################
+
+    '#### Ranking der Suchbegriffe 1 und 2 im Vergleich'
+    st.line_chart(data=ranking, x='Woche', y=["Suchbegriff 1","Suchbegriff 2"])
+
+######################
