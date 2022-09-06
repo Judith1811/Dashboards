e84f08b8dd466034a9711bf35ea49ec978576dce
Judith1811 <judith.paassen@study.hs-duesseldorf.de>
2022-09-06 16:02:18 +0200

    update

--- Dashboards/Status_Quo_Persil.py
+++ Dashboards/Status_Quo_Persil.py
@@ -10,13 +10,9 @@ st.write('Für folgende Auswertung den aktuellen Report aus Vendor Central (http
          'Auswertungsbereich frei wählbar und Abteilung "Amazon.de" oder "Drugstore" möglich.')
 
 uploaded_file = st.file_uploader('Report aus Vendor Central hier hochladen:', type='xlsx')
-#if st.button('Start'):
 if uploaded_file is not None:
-    #uploaded_file.seek(0)
     st.markdown('---')
-    #text_io = io.TextIOWrapper(uploaded_file)
     df = pd.read_excel(uploaded_file, engine='openpyxl')
-    #text_io.seek(0)
     new_header = df.iloc[0]
     df = df[1:]
     df.columns = new_header
@@ -33,26 +29,43 @@ if uploaded_file is not None:
         choice1 = st.radio('Wähle eine Henkel Marke',('Persil','Somat','WC Frisch','Perwoll','Weißer Riese','Spee'))
 
     with col2:
-        choice2 = st.radio('Wähle eine Wettbewerbermarke',('Ariel','Finish'))
+        choice2 = st.radio('Wähle eine Wettbewerbermarke',('Ariel','Finish','Coral','WC Ente','Frosch'))
     #Hilfestellungen###################################################################################
+    choice1dollar = choice1+'$'
+    choice1dollar2 = '$'+choice1
+    choice1space = choice1+ ' '
+    choice1space2 = ' '+choice1
+    choice1combined = [choice1, choice1dollar, choice1dollar2, choice1space, choice1space2]
+    c1= '|'.join(choice1combined)
+    c1 = '"'+c1+'"'
 
-    dsi = df.Suchbegriff.str.contains(choice1, flags = re.IGNORECASE, regex = True, na = False)
+    dsi = df.Suchbegriff.str.contains(c1, flags = re.IGNORECASE, regex = True, na = False)
     toppersil = df[dsi]
     toppersil = toppersil.dropna()
     toppersil.drop(columns=['Abteilung'],inplace=True)
+    toppersil.reset_index(inplace = True)
 
+    choice2dollar = choice2+'$'
+    choice2dollar2 = '$'+choice2
+    choice2space = choice2+ ' '
+    choice2space2 = ' '+choice2
+    choice2combined = [choice2, choice2dollar, choice2dollar2, choice2space, choice2space2]
+    c2= '|'.join(choice2combined)
+    c2 = '"'+c2+'"'
 
-    dsi = df.Suchbegriff.str.contains(choice2, flags = re.IGNORECASE, regex = True, na = False)
+    dsi = df.Suchbegriff.str.contains(c2, flags = re.IGNORECASE, regex = True, na = False)
     topariel = df[dsi]
     topariel = topariel.dropna()
     topariel.drop(columns=['Abteilung'],inplace=True)
+    topariel.reset_index(inplace = True)
 
-    pattern = [choice1, choice2]
+    pattern = [c1, c2]
     pattern2= '|'.join(pattern)
     dsi = df.Suchbegriff.str.contains(pattern2, flags = re.IGNORECASE, regex = True, na = False)
     toppersilariel = df[dsi]
     toppersilariel = toppersilariel.dropna()
     toppersilariel.drop(columns=['Abteilung'],inplace=True)
+    toppersilariel.reset_index(inplace = True)
     ###################################################################################################
 
     col1, col2 = st.columns(2)
@@ -93,14 +106,6 @@ if uploaded_file is not None:
     st.dataframe(toppersilariel)
 
     '#### Henkel Marke unter den TOP 3 der angeklickten ASINs:'
-    dollar = '$'
-    space = ' '
-    choice1dollar = choice1+dollar
-    choice1space = choice1+space
-    choice1space2 = space+choice1
-    choice1combined = [choice1, choice1dollar, choice1space, choice1space2]
-    c1= '|'.join(choice1combined)
-
     df.rename(columns={"Produkttitel #1": "col1","Produkttitel #2": "col2","Produkttitel #3": "col3"},inplace=True)
     ids_persil = df.col1.str.contains(c1, flags = re.IGNORECASE, regex = True, na = False)
     product1_persil = df[ids_persil]
@@ -114,12 +119,6 @@ if uploaded_file is not None:
     st.dataframe(products_persil)
 
     '#### Wettbewerbermarke unter den TOP 3 der angeklickten ASINs:'
-    choice2dollar = choice2+dollar
-    choice2space = choice2+space
-    choice2space2 = space+choice2
-    choice2combined = [choice2, choice2dollar, choice2space, choice2space2]
-    c2= '|'.join(choice2combined)
-
     ids_ariel = df.col1.str.contains(c2, flags = re.IGNORECASE, regex = True, na = False)
     product1_ariel = df[ids_ariel]
     ids_ariel = df.col2.str.contains(c2, flags = re.IGNORECASE, regex = True, na = False)
@@ -134,7 +133,7 @@ if uploaded_file is not None:
     col1, col2 = st.columns(2)
 
     with col1:
-        st.caption('Generische Suchbegriffe, bei denen unter den TOP 3 geklickten Produkten nur Persil auftaucht und kein Ariel')
+        st.caption('Generische Suchbegriffe, bei denen unter den TOP 3 geklickten Produkten nur die Henkel Marke auftaucht und nicht die Wettbewerbermarke')
         vergleich_persil = products_persil.iloc[: , [0,1]]
         vergleich_persil['Marke'] = choice1
         vergleich_persil = vergleich_persil[~vergleich_persil.Suchbegriff.str.contains("persil ")]
@@ -144,7 +143,7 @@ if uploaded_file is not None:
         vergleich_persil
 
     with col2:
-        st.caption('Generische Suchbegriffe, bei denen unter den TOP 3 geklickten Produkten nur Ariel auftaucht und kein Persil')
+        st.caption('Generische Suchbegriffe, bei denen unter den TOP 3 geklickten Produkten nur die Wettbewerbermarke auftaucht und nicht die Henkel Marke')
         vergleich_ariel = products_ariel.iloc[: , [0,1]]
         vergleich_ariel['Marke'] = choice2
         vergleich_ariel = vergleich_ariel[~vergleich_ariel.Suchbegriff.str.contains("ariel ")]
@@ -170,6 +169,11 @@ if uploaded_file is not None:
     waschmittelpodssum.replace(np.nan,'Others',inplace=True)
     labels = waschmittelpodssum['Produkt'].value_counts().index
     values = waschmittelpodssum['Klickrate'].values
+
+    bridge = df.loc[(df['Suchbegriff'] == title)]
+    bridge.reset_index(drop=True,inplace=True)
+    st.write('Der Suchbegriff',title, 'belegt im Ranking Platz', bridge.loc[0]['Suchfrequenz-Rang '])
+
     fig = go.Figure(
         go.Pie(
             labels = labels,
@@ -178,6 +182,7 @@ if uploaded_file is not None:
             textinfo = "percent"
         ))
     st.plotly_chart(fig)
+    ###################################################################################################
 
 
 
